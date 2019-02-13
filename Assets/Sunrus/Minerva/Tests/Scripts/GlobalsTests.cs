25aabf7633861cae333c129b8a3a527fabcd99c2
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2019-02-13 21:43:50 +0200

    Added a very basic test for data resetting

---
+++ Assets/Sunrus/Minerva/Tests/Scripts/GlobalsTests.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+using NUnit.Framework;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Sunrus.Minerva.Tests
+{
+    public class GlobalsTests
+    {
+    
+        [Test]
+        public void ResetTest ()
+        {
+            const string dataPath = "Assets/Sunrus/Minerva/Tests/Resources/Globals/TestGlobalFloat.asset";
+            var testFloat = AssetDatabase.LoadAssetAtPath<GlobalFloat> (dataPath);
+            Debug.Log (testFloat);
+            var value = Random.Range (float.MinValue, float.MaxValue);
+            testFloat.RuntimeValue = value;
+            testFloat.Reset ();
+            
+            Assert.IsTrue (
+                testFloat.InitialValue == testFloat.RuntimeValue,
+                $"{testFloat.InitialValue.ToString()} | {testFloat.RuntimeValue.ToString()} ");
+        }
+    }
+
+}
